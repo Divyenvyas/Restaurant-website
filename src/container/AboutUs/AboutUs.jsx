b196748e2fa44ae1327511615b0c79bc8f6798b1
Divyenvyas <102228616+Divyenvyas@users.noreply.github.com>
2023-04-17 22:08:57 +0530

    Add files via upload

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {images} from '../../constants';
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt='g letter'/>
+
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} className='spoon__img'alt='spoon'/>
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, doloribus aut eaque veritatis inventore illo in natus aliquid quibusdam dolorum ullam totam quasi magni ab voluptatum consectetur dolor id excepturi.
+        </p>
+        <button type='button' className='custom__button'>Know more</button>
+      </div>
+      
+       
+       <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='aboutus__knife'/>
+       </div>
+
+
+       <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} className='spoon__img'alt='spoon'/>
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, doloribus aut eaque veritatis inventore illo in natus aliquid quibusdam dolorum ullam totam quasi magni ab voluptatum consectetur dolor id excepturi.
+        </p>
+        <button type='button' className='custom__button'>Know more</button>
+      </div>
+
+
+    </div>
+  </div>
+);
+
+export default AboutUs;
